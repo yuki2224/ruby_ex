9351afd399395cb791e2a9880e99b37815f065dd
sonoda <xxx.2224.xxx@gmail.com>
2016-06-13 21:00:45 +0900

    ex0702

---
+++ ex0702.rb
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+puts String.new("ようこそ");
+puts String.new("welcome");
+
+print "test\n";
+print "ここが
+文字列\n";
+print "ABC" "DEF\n";
+
+h = "Hello";
+w = "World";
+
+puts h + w;
+
+puts "Hello" << "World";
+
+h = "Hello";
+w = "World";
+
+puts h .concat(w);
+puts h;
